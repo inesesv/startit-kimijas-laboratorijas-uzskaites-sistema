b6ec9293fa96762bea56c18e1054cb1ef00b701f
inesesv <inese.vilumsone@citadaskola.lv>
2020-12-29 09:20:45 +0000

    pievieno komentārus un izveidots routs sveiki

--- serveris.py
+++ serveris.py
@@ -1,13 +1,21 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+app = Flask(__name__)#ģenerē aplikāciju
 
 
-@app.route('/')
-def index():
-    return "Labrīt!"
+@app.route('/')#ceļš uz adresēm
+def index0():#funkcijas nosaukums
+  return "Labrīt!*"
+
+@app.route('/sveiki')
+def index1():
+  return "Nav vairs nekāds rīts!"
+@app.route('/sveiki/<vards>')
+def index2():
+  return "Sveiki <vards"
+
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", debug=True)
+    app.run("0.0.0.0", debug=True)#resartē web serveri šīs ir pēdējās rindiņas vienmēr
