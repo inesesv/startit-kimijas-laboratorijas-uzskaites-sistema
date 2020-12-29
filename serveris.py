8a3522f02e67d6102049f2bc10b741f77a70829a
Inese <inese.vilumsone@citadaskola.lv>
2020-12-29 10:12:47 +0000

    pievienotas vielas

--- serveris.py
+++ serveris.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+import dati
 
 
 app = Flask(__name__)
-
+app.config['JSON_AS_ASCII']=False#atgriež garumzīmes
 
 @app.route('/')
 def index():
-    return "Labrīt!"
+  print(dati.vielas)
+  return "Labrīt!"
+  
+@app.route('/api/v1/vielas')
+def vielas():
+  return jsonify(dati.vielas)#pārvērš par string
 
+@app.route('/api/v1/viela/<vielasID>')
+def viela1(vielasID):
+  viela=f"Vielas ar doto id: {vielasID} nav atrasta" 
+  for v in dati.vielas:
+   if str(v['id'])==vielasID:# var būt arī  if v['id']==int(vielasID):
+     viela=v
+   return jsonify(viela)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
